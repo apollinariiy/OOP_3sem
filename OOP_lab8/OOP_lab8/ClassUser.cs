a673f42bf5dce458acec3d06f8e7249913cacf0f
apollinariy <polinaglushin@gmail.com>
2022-11-09 01:04:40 +0300

    8.1 delegate, event

---
+++ OOP_lab8/OOP_lab8/ClassUser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab8
+{
+
+    public delegate void Fine(string message);
+    /* public event Fine fine;*/
+    public delegate void Increase(string message);
+
+
+
+    class Director {
+        public int salary;
+        public string post;
+        public Director(string pos, int sal)
+        {
+            salary = sal;
+            post = pos;
+        }
+
+        public event Increase increase;
+        
+        public void IncreaseSalary(int _salary)
+        {
+            salary += _salary;
+            if (increase != null)
+            {
+                increase?.Invoke($"Зарплата повышена на {_salary} рублей для {post}");
+            }
+        }
+        public event Fine fine;
+        public void FineSalary(int _salary)
+        {
+            salary -= _salary;
+            if (fine != null)
+            {
+                fine($"Штраф на {_salary} рублей для {post}");
+            }
+        }
+
+
+      
+        public override string ToString()
+        {
+            Console.WriteLine("Post: " + post + " Salary: " + salary);
+            return "";
+        }
+
+    }
+   
+    class kdmkd {
+        
+    }
+}
---
+++ OOP_lab8/OOP_lab8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace OOP_lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director worker = new Director("Director", 1000);
+            Director worker1 = new Director("Student", 100);
+            Director worker2 = new Director("Programmer", 1500);
+            Director worker3 = new Director("Electrician", 500);
+            Director worker4 = new Director("Builder", 600);
+
+            
+            Console.WriteLine("\n--------------------------Cобытия--------------------------\n");
+
+
+            //объект подписан на два события
+            worker2.increase += message => Console.WriteLine(message); //Установка в качестве обработчика лямбда-выражения
+            worker2.fine += message => Console.WriteLine(message);
+            worker.increase += message => Console.WriteLine(message); 
+            worker.fine += message => Console.WriteLine(message);
+
+            //объект подписан на одно событие
+            worker1.fine += message => Console.WriteLine(message);
+            worker3.increase += message => Console.WriteLine(message);
+
+            //для объекта worker2 вызываются оба события
+            worker2.IncreaseSalary(200);
+            worker2.FineSalary(100);
+            
+            worker.FineSalary(500);
+            worker.IncreaseSalary(10);
+
+            //для объекта worker1 вызывается только одно событие
+            worker1.FineSalary(10);
+
+            worker3.IncreaseSalary(300);
+
+            Console.WriteLine("\n--------------------------Состояние объектов после наступления событий--------------------------\n");
+            worker.ToString();
+            worker1.ToString();
+            worker2.ToString();
+            worker3.ToString();
+            worker4.ToString();
+            Console.ReadKey();
+
+/*            void DisplayMessage(string message) => Console.WriteLine(message);//обработчик*/
+           
+
+
+        }
+    }
+}
