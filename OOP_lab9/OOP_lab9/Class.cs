7986ebe9eab5036d934f7638b1ffc4d74d1aaddb
apollinariy <polinaglushin@gmail.com>
2022-11-10 20:41:07 +0300

    9.1 collection, methods, interface

---
+++ OOP_lab9/OOP_lab9/Class.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab9
+{
+    interface IOrderedDictionary
+    {
+        void Add(int key, Car value); // добавление элемента в коллекцию
+        void Clear(); // очистка коллекции
+        bool Contains(Car value); // проверка наличия элемента в коллекции
+        void Remove(int key); // удаление элемента из коллекции
+        int Count { get; } // количество элементов в коллекции
+        ICollection Keys { get; } // возвращает список ключей
+        ICollection Values { get; } // возвращает список значений
+        
+    }
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        
+        public Car(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+    }
+    public class CarDictionary: IOrderedDictionary
+    {
+        public Dictionary<int, Car> list { get; set; }
+        public CarDictionary()
+        {
+            list = new Dictionary<int, Car>();
+        }
+        public int Count => list.Count; // количество элементов в коллекции
+        public void Clear()
+        {
+            list.Clear();
+        }
+        public void Print()
+        {
+            foreach (KeyValuePair<int, Car> item in list)
+                Console.WriteLine("{0}. {1} – {2}$", item.Key, item.Value.Name, item.Value.Price);
+        }
+        public void Add(int key, Car value)
+        {
+            list.Add(key, value);
+        }
+        public void Remove(int key)
+        {
+            list.Remove(key);
+        }
+        public bool Contains(Car carSearch)
+        {
+                if(list.FirstOrDefault(x => x.Value == carSearch).Value != null)  { return true; }
+            return false;
+        }
+      
+        public ICollection Keys => list.ToArray();
+
+        public ICollection Values => list.ToArray();
+    }
+
+}
---
+++ OOP_lab9/OOP_lab9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using OOP_lab9;
+
+namespace OOP_Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// ===============  Инициализация экземпляров Car =================
+            Car car1 = new Car("Mers", 4800);
+            Car car2 = new Car("Nissan", 19000);
+            Car car3 = new Car("BMW", 3900);
+            Car car4 = new Car("Toyota", 16500);
+
+
+            /// =========  Методы для работы с коллекцией (словарём)  ==========
+            CarDictionary showRoom = new CarDictionary();
+            showRoom.Add(1, car1);
+            showRoom.Add(2, car2);
+            showRoom.Add(3, car3);
+            showRoom.Add(4, car4);
+            showRoom.Remove(3);
+            Console.WriteLine(new String('=', 60));
+            Console.WriteLine("\n----------------------------Объекты слловаря:--------------------------------\n");
+            showRoom.Print();
+            Console.WriteLine("Резултат поиска объекта {0} в словаре:{1}\n", car3.Name, showRoom.Contains(car3));
+        }
+    }
+}
