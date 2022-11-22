8ffd4f6bcdaafc7a9de9e3e8c1c2c631c8ac41e8
apollinariy <polinaglushin@gmail.com>
2022-11-22 16:55:37 +0300

    14.2 working with threads

--- OOP_lab14/OOP_lab14/Program.cs
+++ OOP_lab14/OOP_lab14/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Diagnostics;
 using System.Reflection;
+using OOP_lab14;
 
 namespace OOP_Lab14
 {
@@ -19,7 +20,7 @@ namespace OOP_Lab14
 
             //ЗАДАНИЕ 2. Исследуйте текущий домен вашего приложения: имя, детали конфигурации, все сборки, загруженные в домен. Создайте новый домен. Загрузите туда сборку. Выгрузите домен.
 
-            /*AppDomain domain = AppDomain.CurrentDomain;
+            AppDomain domain = AppDomain.CurrentDomain;
             Console.WriteLine($"\n------------------------------Текущий домен------------------------------\n");
             Console.WriteLine($"Name: {domain.FriendlyName} \nBase Directory: {domain.BaseDirectory} \nConfiguration File: {domain.SetupInformation}");
             Assembly[] assemblies = domain.GetAssemblies();
@@ -27,11 +28,40 @@ namespace OOP_Lab14
             foreach (Assembly assembly in assemblies)
             {
                 Console.WriteLine(assembly.GetName().Name);
-            }*/
+            }
 
-            var newDomain = AppDomain.CreateDomain("Domain");
-            newDomain.Load(Assembly.GetExecutingAssembly().GetName());
-            AppDomain.Unload(newDomain);
+          /*  var newDomain = AppDomain.CreateDomain("Domain"); ??????
+            newDomain.Load(Assembly.GetExecutingAssembly().GetName());//Creating and unloading AppDomains is not supported and throws an exception.??
+            AppDomain.Unload(newDomain);*/
+
+            //ЗАДАНИЕ 3. Создайте в отдельном потоке следующую задачу расчета.
+            
+            Thread threadNumber = new Thread(Tasks.Numbers);
+            threadNumber.Start();
+            Console.WriteLine($"\n------------------------------Информация о потоке------------------------------\n");
+            Console.WriteLine($"Name: {threadNumber.Name} | Priority: {threadNumber.Priority} | ID: {threadNumber.ManagedThreadId} | Status: {threadNumber.ThreadState}");
+            threadNumber.Join();
+
+            //ЗАДАНИЕ 4. Создайте два потока. Первый выводит четные числа, второй нечетные до n и записывают их в общий файл и на консоль. Скорость расчета чисел у потоков – разная.
+
+
+            Thread evenThread = new Thread(Tasks.evenNumbers);
+            evenThread.Priority = ThreadPriority.AboveNormal;          
+            evenThread.Start();             
+            /*evenThread.Join();*///закоментировать для работы потоков вместе             
+
+            Console.WriteLine();
+            Thread oddThread = new Thread(Tasks.oddNumbers);
+            oddThread.Priority = ThreadPriority.BelowNormal;
+            oddThread.Start();
+            oddThread.Join();
+
+           
+
+
+
+
+
         }
     }
 }
--- OOP_lab14/OOP_lab14/Task.cs
+++ OOP_lab14/OOP_lab14/Task.cs
@@ -6,11 +6,102 @@ using System.Threading.Tasks;
 
 namespace OOP_lab14
 {
-    public static class Task
+    public static class Tasks
     {
-        //записи в файл и на консоль простых чисел от 1 до n (задает пользователь)
-        public static void Numbers() { 
+        public static void Numbers()
+        {
+            Console.WriteLine("Введите n:");
+            int n = int.Parse(Console.ReadLine());
+
+            using (StreamWriter sw = new StreamWriter(@"D:\3sem\ООП\лабы\лр1\OOP_3sem\OOP_lab14\numbers.txt", false, System.Text.Encoding.Default))
+            {
+                for (var i = 1; i <= n; i++)
+                {
+                    sw.WriteLine(i);
+                    Thread.Sleep(300);
+                }
+            }
+
+            for (var i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(300);
+
+            }
+        }
+        public static void evenNumbers()
+        {
+
+            using (StreamWriter sw = new StreamWriter(@"D:\3sem\ООП\лабы\лр1\OOP_3sem\OOP_lab14\EvenOddNumbers.txt", false, System.Text.Encoding.Default))
+            {
+                for (var i = 1; i <= 12; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sw.WriteLine(i);
+                        Thread.Sleep(600);
+                    }
+                }
+            }
+            for (var i = 1; i <= 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + " четное");
+                    Thread.Sleep(400);
+                }
+            }
             
         }
+        public static void oddNumbers()
+        {
+
+            using (StreamWriter sw = new StreamWriter(@"D:\3sem\ООП\лабы\лр1\OOP_3sem\OOP_lab14\EvenOddNumbers.txt", true, System.Text.Encoding.Default))
+            {
+                for (var i = 1; i <= 12; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sw.WriteLine(i);
+                        Thread.Sleep(400);
+                    }
+                }
+            }
+            for (var i = 1; i <= 12; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(600);
+                }
+            }
+        }
+
+       /* public static void WriteData()
+        {
+            using (StreamWriter sw = new StreamWriter(@"D:\3sem\ООП\лабы\лр1\OOP_3sem\OOP_lab14\EvenOddNumbers.txt", true, System.Text.Encoding.Default))
+            {
+                for (var i = 1; i <= 12; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sw.WriteLine(i);
+                        Thread.Sleep(400);
+                    }
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i + " четное");
+                        Thread.Sleep(400);
+                    }
+                }
+
+            }
+
+        }*/
+
+
     }
+   
 }
+
+
