086eed075e3aea53d603d5c56e90786aa4251f1c
apollinariy <polinaglushin@gmail.com>
2022-11-25 22:26:11 +0300

    11.2 demonstration Reflector, Create()

--- OOP_lab11/OOP_lab11/Program.cs
+++ OOP_lab11/OOP_lab11/Program.cs
@@ -8,18 +8,19 @@ using System.Threading.Tasks;
 
 namespace OOP_lab11
 {
+    public class Object { }
     class Program
     {
         static void Main(string[] args)
         {
             Program program = new Program();
+            Console.WriteLine("\n--------------------------------- Reflector for class Test ---------------------------------\n");
             Console.WriteLine("\t---- СБОРКА: ----");
             Reflector.GetName("OOP_lab11.Test");
             Console.WriteLine("\t---- КОНСТРУКТОРЫ: ----");
             Reflector.GetConstructors("OOP_lab11.Test");
             Console.WriteLine("\t---- МЕТОДЫ: ----");
             Reflector.GetMethod("OOP_lab11.Test");
-            Reflector.GetMethod("OOP_lab11.Program");
             Console.WriteLine("\t---- CВОЙСТВА И ПОЛЯ: ----");
             Reflector.GetField("OOP_lab11.Test");
             Console.WriteLine("\t---- ИНТЕРФЕЙСЫ: ----");
@@ -29,11 +30,61 @@ namespace OOP_lab11
             Reflector.MethodForType("OOP_lab11.Test", "String");
             Console.WriteLine();
             Console.WriteLine("\t---- СЧИТЫВАНИЕ ИНФОРМАЦИИ  ИЗ ФАЙЛА: ----");
-            Reflector.Invoke("OOP_lab11.Test", "Toconsole");
-            Console.WriteLine();
+           /* Reflector.Invoke("OOP_lab11.Test", "Toconsole");
+            Console.WriteLine();*/
             Console.WriteLine("\t---- СОЗДАНИЕ ОБЪЕКТА ПЕРЕДАННОГО ТИПА: ----");
-            Reflector.Create("OOP_lab11.Test", "Kristina");
-            Console.ReadKey();
+            Reflector.Create("OOP_lab11.Test", "Polina");
+
+
+            Console.WriteLine("\n\n--------------------------------- Reflector for class Trans ---------------------------------\n");
+            Console.WriteLine("\t---- СБОРКА: ----");
+            Reflector.GetName("OOP_lab11.Trans");
+            Console.WriteLine("\t---- КОНСТРУКТОРЫ: ----");
+            Reflector.GetConstructors("OOP_lab11.Trans");
+            Console.WriteLine("\t---- МЕТОДЫ: ----");
+            Reflector.GetMethod("OOP_lab11.Trans");
+            Console.WriteLine("\t---- CВОЙСТВА И ПОЛЯ: ----");
+            Reflector.GetField("OOP_lab11.Trans");
+            Console.WriteLine("\t---- ИНТЕРФЕЙСЫ: ----");
+            Reflector.GetInterfece("OOP_lab11.Trans");
+            Console.WriteLine("\t---- МЕТОДЫ ПО ПАРАМЕТРУ: ----");
+            Reflector.MethodForType("OOP_lab11.Trans", "String");
+            Console.WriteLine();
+
+            Console.WriteLine("\n\n--------------------------------- Reflector for class Human ---------------------------------\n");
+            Console.WriteLine("\t---- СБОРКА: ----");
+            Reflector.GetName("OOP_lab11.Human");
+            Console.WriteLine("\t---- КОНСТРУКТОРЫ: ----");
+            Reflector.GetConstructors("OOP_lab11.Human");
+            Console.WriteLine("\t---- МЕТОДЫ: ----");
+            Reflector.GetMethod("OOP_lab11.Human");
+            Console.WriteLine("\t---- CВОЙСТВА И ПОЛЯ: ----");
+            Reflector.GetField("OOP_lab11.Human");
+            Console.WriteLine("\t---- ИНТЕРФЕЙСЫ: ----");
+            Reflector.GetInterfece("OOP_lab11.Human");
+            Console.WriteLine("\t---- МЕТОДЫ ПО ПАРАМЕТРУ: ----");
+            Reflector.MethodForType("OOP_lab11.Human", "String");
+            Console.WriteLine();
+
+            Console.WriteLine("\n\n--------------------------------- Reflector for System.Object ---------------------------------\n");
+            Console.WriteLine("\t---- СБОРКА: ----");
+            Reflector.GetName("OOP_lab11.Object");
+            Console.WriteLine("\t---- КОНСТРУКТОРЫ: ----");
+            Reflector.GetConstructors("System.Object");
+            Console.WriteLine("\t---- МЕТОДЫ: ----");
+            Reflector.GetMethod("System.Object");
+            Console.WriteLine("\t---- CВОЙСТВА И ПОЛЯ: ----");
+            Reflector.GetField("System.Object");
+            Console.WriteLine("\t---- ИНТЕРФЕЙСЫ: ----");
+            Reflector.GetInterfece("System.Object");
+            Console.WriteLine("\t---- МЕТОДЫ ПО ПАРАМЕТРУ: ----");
+            Reflector.MethodForType("System.Object", "String");
+            Console.WriteLine();
+
+
+
+
+
         }
     }
 }
